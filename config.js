29c7a322b9b9cdd12860170d8ed744621ea958d7
TusProtein <maianhtu1111@gmail.com>
2023-10-31 14:42:14 +0700

    Create folders routes and app/controller

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = () =>
+  (module.exports = {
+    key: "value",
+  });
+export default config;
--- src/index.js
+++ src/index.js
@@ -3,34 +3,35 @@ import morgan from "morgan";
 import { engine as handlebars } from "express-handlebars";
 import path from "path";
 import { fileURLToPath } from "url";
+import route from "./routes/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// const express = require("express");
-// const morgan = require("morgan");
-// const handlebars = require("express-handlebars");
-// const path = require("path");
 
 const app = express();
 
 //Thay đổi path name
 app.use(express.static(path.join(__dirname, "public")));
 
+//Parse body request
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//Parse Frameworks JS(fetch, axios,...)
+app.use(express.json());
+
 //HTTP Logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 //Template engines
 app.engine("hbs", handlebars({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Route app
+route(app);
 
 app.listen(3000);
