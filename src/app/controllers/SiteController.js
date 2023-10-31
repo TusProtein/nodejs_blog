e6afc190533f84fe9cdf1235ee567480d4f1f30a
TusProtein <maianhtu1111@gmail.com>
2023-10-31 17:56:18 +0700

    Fix Extension Prettier

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,11 +1,11 @@
 class SiteController {
     //[GET] /news
     index(req, res) {
-        res.render("home");
+        res.render('home');
     }
     //[GET] /:slug
     search(req, res) {
-        res.render("search");
+        res.render('search');
     }
 }
 
